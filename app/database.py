a7351dcf63d76cd81f61223c1f1c6dfa911f2783
donglehae <dongledh@gmail.com>
2022-07-08 17:19:05 +0900

    fix: Remove mysql-connector

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
-SQLALCHEMY_DATABASE_URL = f'mariadb+mariadbconnector://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URL = f'mariadb://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?charset=utf8mb4'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
